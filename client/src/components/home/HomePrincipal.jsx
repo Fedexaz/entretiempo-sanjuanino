749f33fc490e928cb60350a8fa933ea503f59066
Fede Lahoz <fedexaz3@gmail.com>
2022-03-29 11:15:05 -0300

    Delete client directory

--- client/src/components/home/HomePrincipal.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HomePrincipal() {
-  return (
-    <div>HomePrincipal</div>
-  )
-}
-
-export default HomePrincipal
--- client/src/redux/actionTypes.js
+++
@@ -1,7 +0,0 @@
-const actions = {
-    GET_PARTIDOS: "GET_PARTIDOS",
-    GET_EQUIPOS: "GET_EQUIPOS",
-    GET_JUGADORES: "GET_JUGADORES"
-}
-
-export default actions
