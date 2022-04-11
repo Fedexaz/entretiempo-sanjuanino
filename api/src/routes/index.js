5dad5dc4b8dc7089e8619bbf316d541c8aacdd30
fedexaz <fedexaz3@gmail.com>
2022-04-11 00:24:12 -0300

    Agrgando rutas para la api

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,20 @@
 const { Router } = require('express');
 const userRoutes = require('./user.route');
 
+const eventRoutes = require('./event.route');
+const matchRoutes = require('./match.route');
+const playerRoutes = require('./player.route');
+const teamRoutes = require('./team.route');
+const tokenAPIRoutes = require('./tokenAPI.route');
+
 const rutas = Router();
 
 rutas.use('/user', userRoutes);
+
+rutas.use('/api/event', eventRoutes);
+rutas.use('/api/match', matchRoutes);
+rutas.use('/api/player', playerRoutes);
+rutas.use('/api/team', teamRoutes);
+rutas.use('/tokenAPI', tokenAPIRoutes);
 
 module.exports = rutas;
